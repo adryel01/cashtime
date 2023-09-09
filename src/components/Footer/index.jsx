fe30d04013ad9e9eba71404b96bd6c4506fd1ced
adryel01 <adryelcesarm@gmail.com>
2023-09-09 16:40:07 -0300

    att domain

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -23,7 +23,7 @@ export function Footer() {
 					<h3>
 						Fale Conosco
 					</h3>
-					<p><b>Email:</b> contato@cashtime.com.br</p>
+					<p><b>Email:</b> contato@cashtimepayments.com.br</p>
 				</div>
 				<div className='RightFooter'>
 					<h3>
