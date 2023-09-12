a16deaf36675a2a381ba661ae8bcd45c41c287c3
adryel01 <adryelcesarm@gmail.com>
2023-09-12 14:39:09 -0300

    put out d+2

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -8,8 +8,8 @@ export function Skills() {
 			<div className='slider'>
 				<div className='slide-track'>
 					<div className='slide'><button>Checkout Personalizado</button></div>
-					<div className='slide'><button>Saque na Hora</button></div>
-					<div className='slide'><button>Pagamento em D+2</button></div>
+					<div className='slide'><button>Saque Rápido</button></div>
+					<div className='slide'><button>Alta Conversão</button></div>
 					<div className='slide'><button>Pix na Hora</button></div>
 					<div className='slide'><button>Recuperação de Carrinho</button></div>
 					<div className='slide'><button>Melhores Taxas do Mercado</button></div>
