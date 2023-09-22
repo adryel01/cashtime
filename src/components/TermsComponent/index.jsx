7e2abf9810a0b33a34614caf70cdbe2550887f21
adryel01 <adryelcesarm@gmail.com>
2023-09-22 17:56:11 -0300

    start terms of use

--- src/components/TermsComponent/index.jsx
+++ src/components/TermsComponent/index.jsx
@@ -6,7 +6,18 @@ export function TermsComponent() {
 
 	return (
 		<div className='TextTerms'>
-			
+			<div className='TermsContainer'>
+				<h1>Termos de Uso</h1>
+				<div>
+					<h3>PREÂMBULO</h3>
+					<h3>DAS PARTES:</h3>
+				</div>
+				<div>
+					<p>São partes deste instrumento:</p>
+					<p>Na qualidade de CONTRATADA: CASHTIME, sediada em GOIÂNIA-GO, representada como CESAR ECOM no seu contrato social.</p>
+					<p>Na qualidade de CONTRATANTE: Empresa que afirmar que leu e assinou estes termos de uso.</p>
+				</div>
+			</div>
 		</div>
 	)
 }
