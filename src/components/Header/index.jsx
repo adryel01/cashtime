6f993d0fa284c5e4f944513fa21ac05d1ba3a7cc
adryel01 <adryelcesarm@gmail.com>
2023-10-14 17:29:38 -0300

    att button function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -27,7 +27,7 @@ export function HeaderContainer() {
 				<img src={cashtime} alt="" />
 			</Link>
 			<nav className="NavHeader">
-				<a href="https://app.gateway.cashtimepay.com.br/register" className='LinkAcessHeader' target='_blank' onClick={gtag_report_conversion('https://app.gateway.cashtimepay.com.br/register')}>Acessar Gateway</a>
+				<a href="https://app.gateway.cashtimepay.com.br/register" className='LinkAcessHeader' target='_blank' onClick={()=>gtag_report_conversion('https://app.gateway.cashtimepay.com.br/register')}>Acessar Gateway</a>
 				<a href='https://app.cashtimepayments.com.br/register' target='_blank'><button className='SignUpButtonHeader'>Acessar Checkout</button></a>
 			</nav>
 		</div>
