e1d4fa83f847ace9f30355bd4bf3d5909e6b6ea3
Adryel <48932475+adryel01@users.noreply.github.com>
2024-01-12 19:06:25 -0300

    Atualizar o index.jsx

--- src/components/Whatsapp/index.jsx
+++ src/components/Whatsapp/index.jsx
@@ -6,7 +6,7 @@ export function Whatsapp(){
 	return (
 
 		<div className='Whatscontainer'>
-			<a href='https://api.whatsapp.com/send?phone=5562998416575&text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20a%20Cashtime!' target='_blank'><img src={whats} alt="" className='Whatsicon'/></a>
+			<a href='https://api.whatsapp.com/send?phone5562996627948=&text=Olá,%20gostaria%20de%20mais%20informações%20sobre%20a%20Cashtime!' target='_blank'><img src={whats} alt="" className='Whatsicon'/></a>
 		</div>
 	)
 }
